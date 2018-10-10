f02cf98e6aa62d11c5b74ca827933c50a381737d
Qifan Guo <qifanguo@gmail.com>
2018-10-10 19:37:50 -0400

    PriceMap data entries complete

--- app/src/main/java/com/qifan/bestwing_pos/Model/ItemPrices.java
+++ app/src/main/java/com/qifan/bestwing_pos/Model/ItemPrices.java
@@ -5,7 +5,9 @@ import java.util.HashMap;
 public class ItemPrices {
     public static HashMap<String,Double> itemPriceMap;
     public Order[] mOrders = new Order[]{canSoda,bottleSoda,medLemonade,largeLemonade,SixWing,TenWing,FifteenWing,TwentyWing,ThirtyWing,FortyWing,
-    FiftyWing,HundredWing,sideFriedRice,sideFries,addBeef,addChicken,addShrimp,addHH,addTY,split,addHouse};
+    FiftyWing,HundredWing,sideFriedRice,sideFries,addBeef,addChicken,addShrimp,addHH,addTY,split,addHouse,gardenSalad,turkeyClub,turkeySalad,hamClub,hamSalad,chefSalad,grilledChickenSalad,
+    fishSalad,regularFriedRice,beefFriedRice,shrimpFriedRice,chickenFriedRice,houseFriedRice,vegFriedRice,phillyChicken,phillySteak,hamburger,cheeseburger,doubleCheeseburger,fishSandwich,
+    gyro,vegRoll,shrimpRoll,steakRoll,twoFish,threeFish,fourFish,fiveFish,sixShrimp,twelveShrimp,fourTender,addTender,smallFries,largeFries,cornDog,crabRagoon};
 
     public ItemPrices(){
         itemPriceMap = new HashMap<>();
@@ -36,6 +38,49 @@ public class ItemPrices {
     public static final Order bottleSoda = new Order("bottleSoda",1.76);
     public static final Order medLemonade = new Order("medLemonade",1.00);
     public static final Order largeLemonade = new Order("largeLemonade",2.00);
+
+    public static final Order gardenSalad = new Order("gardenSalad",5.59);
+    public static final Order turkeySalad = new Order("turkeySalad",5.59);
+    public static final Order hamSalad = new Order("hamSalad",5.59);
+    public static final Order chefSalad = new Order("chefSalad",5.59);
+    public static final Order grilledChickenSalad = new Order("grilledChickenSalad",5.99);
+    public static final Order fishSalad = new Order("fishSalad",6.99);
+    public static final Order regularFriedRice = new Order("regularFriedRice",3.99);
+    public static final Order chickenFriedRice = new Order("chickenFriedRice",5.59);
+    public static final Order beefFriedRice = new Order("beefFriedRice",5.59);
+    public static final Order shrimpFriedRice = new Order("shrimpFriedRice",6.59);
+    public static final Order houseFriedRice = new Order("houseFriedRice",7.99);
+    public static final Order vegFriedRice = new Order("vegFriedRice",5.19);
+    public static final Order phillySteak = new Order("phillySteak",4.99);
+    public static final Order phillyChicken = new Order("phillyChicken",4.99);
+    public static final Order hamburger = new Order("hamburger",3.69);
+    public static final Order cheeseburger = new Order("cheeseburger",3.99);
+    public static final Order doubleCheeseburger = new Order("doubleCheeseburger",5.19);
+    public static final Order fishSandwich = new Order("fishSandwich",3.99);
+    public static final Order gyro = new Order("gyro",4.99);
+    public static final Order hamClub = new Order("hamClub",3.99);
+    public static final Order turkeyClub = new Order("turkeyClub",3.99);
+    public static final Order vegRoll = new Order("vegRoll",1.29);
+    public static final Order shrimpRoll = new Order("shrimpRoll",1.39);
+    public static final Order  steakRoll= new Order("steakRoll",1.99);
+    public static final Order  twoFish= new Order("twoFish",6.49);
+    public static final Order  threeFish= new Order("threeFish",8.49);
+    public static final Order  fourFish= new Order("fourFish",10.49);
+    public static final Order  fiveFish= new Order("fiveFish",12.49);
+    public static final Order sixShrimp = new Order("sixShrimp",7.99);
+    public static final Order  twelveShrimp= new Order("twelveShrimp",11.99);
+    public static final Order  fourTender= new Order("fourTender",5.69);
+    public static final Order  addTender= new Order("addTender",1.29);
+    public static final Order smallFries = new Order("smallFries",1.59);
+    public static final Order  largeFries= new Order("largeFries",2.99);
+    public static final Order  cornDog = new Order("cornDog",1.00);
+    public static final Order  crabRagoon = new Order("crabRagoon",1.39);
+
+
+
+
+
+
 
 
 
